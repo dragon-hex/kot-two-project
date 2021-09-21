aed8370815d564746acbd037b2c47238c5fdb808
dragon-hex <infinityuser0101@gmail.com>
2021-09-21 09:03:31 -0300

    new content

--- kot2/system/content.py
+++ kot2/system/content.py
@@ -1,3 +1,34 @@
+# as like all the modules, import pygame
+import pygame
+# -- begin content storage & provider class:
+# provides a image or some asset for the game
+# and keeps it in memory.
+
+CONTENT_TYPE_IMAGE      = 1
+CONTENT_TYPE_FONT       = 2
+
 class content:
     def __init__(self):
-        self.game_path  = None
+        # keep on the cache some stuff
+        # for a determined amount of time.
+        self.game_path      = None
+        self.cache          = {
+            # CACHE STORE: <NAME_CACHE_REF>: [<CACHE_DATA>, <LIFESPAN_TIME>]
+            # NOTE: -1 is permanent on the cache.
+            'null': [0, -1]
+        }
+    
+    def get_content(self, name, what_type, **kwargs):
+        """
+        this will get the content and store on the cache
+        but you need to provide the type.
+
+        font_size: case you using fonts.
+        """
+        font_size   = kwargs.get("font_size") or 12
+        if what_type == CONTENT_TYPE_IMAGE:  cache_name = "img_%d" % name
+        elif what_type == CONTENT_TYPE_FONT: cache_name = "font_%d_%d" % (name, font_size)
+        if self.cache.get(cache_name):
+            # NOTE: when the cache is used again, put a 30 seconds more in the memory.
+            self.cache[cache_name][1] += (30 * 1000)
+            return self.cache.get(cache_name)[0]
