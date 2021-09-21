ee27949df67e243a17f26e021686d8c2b5b4dd87
dragon-hex <infinityuser0101@gmail.com>
2021-09-21 20:15:12 -0300

    new engine content provider infraesture

--- kot2/system/content.py
+++ kot2/system/content.py
@@ -1,5 +1,9 @@
 # as like all the modules, import pygame
 import pygame
+# import some other modules
+from time import gmtime, strftime
+import os
+
 # -- begin content storage & provider class:
 # provides a image or some asset for the game
 # and keeps it in memory.
@@ -7,16 +11,94 @@ import pygame
 CONTENT_TYPE_IMAGE      = 1
 CONTENT_TYPE_FONT       = 2
 
+# list of possible errors
+RESOURCE_NOT_FOUND      = -1
+ERROR_OPENING_RESOURCE  = -2
+
+# some constants (that can be changed on the init)
+AVG_TIME_IN_CACHE       = 30 * 1000
+DEBUG_CORE              = False
+
 class content:
     def __init__(self):
         # keep on the cache some stuff
         # for a determined amount of time.
-        self.game_path      = None
-        self.cache          = {
+        self.__output           = None
+        self.__output_enabled   = False
+        self.game_path          = None
+        self.cache              = {
             # CACHE STORE: <NAME_CACHE_REF>: [<CACHE_DATA>, <LIFESPAN_TIME>]
             # NOTE: -1 is permanent on the cache.
             'null': [0, -1]
         }
+    
+    def __handle_exception(self, at, exception):
+        """ if you hooked a function to the listener... """
+        # TODO: finish this.
+        pass
+
+    def __get_timestamp(self):
+        """ return the time written something (function mostly used by the log function) """
+        return strftime("%Y/%m/%d %H:%M:%S",gmtime())
+    
+    def __write_log(self, the_log):
+        """ write some log to the output. """
+        if self.__output_enabled:
+            text = "(%s) content-provider: %s" % (self.__get_timestamp(),the_log)
+            if DEBUG_CORE:
+                # NOTE: this is right, we want to know if some problem
+                # happen during the write time.
+                self.__output.write(text+"\n")
+            else:
+                try:
+                    self.__output.write(text+"\n")
+                except Exception as E:
+                    self.__handle_exception("__write_log()",E)
+    
+    def __sandbox_action(self, function, function_name):
+        """ function to make the functions smaller, dummi. """
+        if DEBUG_CORE:
+            return function()
+        else:
+            try:
+                return function()
+            except Exception as E:
+                self.__handle_exception(function_name,E)
+
+    def __get_image_raw(self, name, use_prefix=None):
+        """ try to import the image and all the stuff """
+        possible_dir    = self.game_path + (use_prefix or "images/") + name + ".png"
+        if not os.path.exists(possible_dir):
+            return RESOURCE_NOT_FOUND, None
+        if DEBUG_CORE:
+            # NOTE: debug doesn't allow to handle certain crash
+            # for better view of the core crashes.
+            images = pygame.image.load(possible_dir)
+        else:
+            try:
+                images = pygame.image.load(possible_dir)
+            except Exception as E:
+                # just in case the function fails to load the image...
+                self.__handle_exception("__get_image_raw()",E)
+                return ERROR_OPENING_RESOURCE, None
+        return 0, images
+    
+    def __get_font_raw(self, name, font_size):
+        """ try to import a new font """
+        possible_dir    = self.game_path + "fonts/" + name + ".ttf"
+        if not os.path.exists(possible_dir):
+            return RESOURCE_NOT_FOUND, None
+        if DEBUG_CORE:
+            # NOTE: this is the same thing on the other functions
+            # that can raise exceptions :-)
+            font = pygame.font.Font(name, font_size)
+        else:
+            try:
+                font = pygame.font.Font(name, font_size)
+            except Exception as E:
+                self.__handle_exception("__get_font_raw()",E)
+        return 0, font
+
     
     def get_content(self, name, what_type, **kwargs):
         """
@@ -26,9 +108,21 @@ class content:
         font_size: case you using fonts.
         """
         font_size   = kwargs.get("font_size") or 12
+        force_dir   = kwargs.get("force_dir") or "images"
         if what_type == CONTENT_TYPE_IMAGE:  cache_name = "img_%d" % name
         elif what_type == CONTENT_TYPE_FONT: cache_name = "font_%d_%d" % (name, font_size)
         if self.cache.get(cache_name):
             # NOTE: when the cache is used again, put a 30 seconds more in the memory.
             self.cache[cache_name][1] += (30 * 1000)
             return self.cache.get(cache_name)[0]
+        else:
+            if what_type == CONTENT_TYPE_IMAGE:
+                status, cache_created = self.__get_image_raw(name, use_prefix=force_dir)
+                # check if the cache was created (aka, the image)
+                if status != 0: return status
+                self.cache[cache_name]=(cache_created,pygame.time.get_ticks()+AVG_TIME_IN_CACHE)
+                return cache_created
+            elif what_type == CONTENT_TYPE_FONT:
+                status, cache_created = self.__get_font_raw(name, font_size)
+                # check if the font was created (aka, the font)
+                if status != 0: return status
