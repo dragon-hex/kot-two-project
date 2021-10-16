9cc622ae545f9d1ee40e49f244ccd6681ded6f16
dragon-hex <infinityuser0101@gmail.com>
2021-10-16 10:41:36 -0300

    new element updates

--- kot2/ui/elements.py
+++ kot2/ui/elements.py
@@ -3,6 +3,9 @@ import pygame
 import kot2.util
 import sys
 
+# import the base module 
+from . import base
+
 # define some project constants.
 DEBUG_THIS_FILE     = True
 
@@ -65,21 +68,68 @@ class style:
         self.visible    = False
 
 # -- display class begin here --
+class cursor:
+    def __init__(self):
+        self.cursor_textures = []
+        self.cursor_state = 0
+        self.cursor_animation_index = 0
+        self.cursor_animation_time = 0
+    
+    def tick(self): 
+        """ tick the cursor for update it animation and time. """
+        if self.cursor_animation_time < pygame.time.get_ticks():
+            if len(self.cursor_textures[self.cursor_state]) < self.cursor_animation_index + 1:
+                self.cursor_animation_index     = 0
+            else:
+                self.cursor_animation_index     += 1
+            self.cursor_animation_time = pygame.time.get_ticks() + (0.5 * 1000)
+
 class display(__content_pool):
     def __init__(self, at_surface):
         """ set the display here. """
+        # pre-init the content pool here
+        super().__init__()
+        # alright, keep __init__ here.
         self.at_surface = at_surface
-        # some custom properties
         self.style = style()
+        self.cursor = cursor()
     
     def tick(self, ev_list):
         """ tick the elements on the screen. """
         if self.style.visible:
+            self.cursor.tick()
             self.tick_element(ev_list)
     
     def draw(self):
         """ draw the elements on the screen """
         if self.style.visible:
             self.draw_element()
+            self.__draw_cursor()
         
 # -- frame class begin here --
+class frame(__content_pool):
+    """
+    frame: stores objects inside it, also, it can have
+    a background.
+    """
+    def __init__(self, at_display):#
+        # pre-init the class to the content pool.
+        super().__init__()
+        # keep the __init__ here :)
+        self.type = "frame"
+        self.style = style()
+        self.background = None
+        self.background_pos = None
+    
+    def new_background(self, background_size):
+        """ generate a new background. """
+        self.background = pygame.Surface(background_size, pygame.SRC_ALPHA)
+    
+    def tick(self, ev_list):
+        """ tick all the elements on the list. """
+        if self.style.visible:
+            self.tick_element(ev_list)
+    
+    def draw(self):
+        if self.style.visible:
+            self.draw_element()
