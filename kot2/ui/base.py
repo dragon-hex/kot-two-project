66e18a0e0814792767519b5ce179d2eb3d6aafb2
dragon-hex <infinityuser0101@gmail.com>
2021-10-16 10:41:30 -0300

    new updates on the base

---
+++ kot2/ui/base.py
@@ -0,0 +1,34 @@
+#
+# CONSTS AND MORE
+#
+
+CURSOR_NORMAL_STATE     = 0
+CURSOR_WAITING_STATE    = 1
+
+#
+# Element Styles
+#
+
+class generic_shared_prop:
+    def __init__(self):
+        """ 
+        this include things like:
+        * visibility
+        * locked
+        * position
+        """
+        self.visible    = True
+        self.locked     = True
+        self.position   = [0, 0]
+
+class label_style(generic_shared_prop):
+    def __init__(self):
+        """
+        this stores the label information about it
+        style such as background color and more.
+        """
+        # load the generic properties.
+        super().__init__()
+        self.background_color   = (  0,   0,   0)
+        self.foreground_color   = (255, 255, 255)
+        self.use_antialising    = True
