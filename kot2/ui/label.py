132054071b50e3e0ecd3bd6140694eecd506c493
dragon-hex <infinityuser0101@gmail.com>
2021-10-23 09:53:32 -0300

    label changes

--- kot2/ui/label.py
+++ kot2/ui/label.py
@@ -9,16 +9,20 @@ class label:
         # secret variables
         self.__abs_position = [0, 0]
         self.__surface = None
+        self.__need_redraw=True
     
     def set_font(self, what_font):
         self.font = what_font
+        self.__need_redraw=True
     
     def set_text(self, what_text):
         self.text = what_text
+        self.__need_redraw=True
     
     def render(self):
         self.__surface = self.font.render(self.Text,self.style.use_antialising,self.style.foreground_color)
         self.__abs_position = [0, 0]
+        self.__need_redraw=False
     
     def tick(self, ev_list):
         # NOTE: tick will also, render the text.
