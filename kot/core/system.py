ed6f0f56cc30a01ff462e35a55be0f7e73c9bc18
dragon-hex <infinityuser0101@gmail.com>
2021-11-01 15:38:52 -0300

    new system is easy and simple to understand.

---
+++ kot/core/system.py
@@ -0,0 +1,38 @@
+# -- import the classes. --
+import pygame
+# -- kotWindow: stores the window.
+class kotWindow:
+    def __init__(self):
+        self.surface = None
+        self.icon = None
+        self.caption = None
+        self.size = None
+    def __applyTheIcon(self):
+        """__applyTheIcon: basically applies the window icon."""
+        if self.icon:
+            try: pygame.display.set_icon(self.icon)
+            except: return False
+            return True
+    def windowSetIcon(self, windowIcon):
+        """windowSetIcon: set the window icon."""
+        self.icon = windowIcon
+        self.__applyTheIcon()
+    def windowInit(self):
+        """windowInit: initializes the window."""
+        self.surface = pygame.display.set_mode(self.size)
+        pygame.display.set_caption(self.caption)
+        self.__applyTheIcon()
+    def windowUpdateProperties(self):
+        """windowUpdateProperties: update the window properties."""
+        pygame.display.set_caption(self.caption)
+        self.__applyTheIcon()
+
+class kotSharedCore:
+    def __init__(self):
+        self.window = kotWindow()
+        self.running = True
+        self.mode = []
+        self.on_mode = 0
+    def getRunning(self):
+        """getRunning: get the state of the game."""
+        return self.running
