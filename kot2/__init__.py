2bd973898f562349a1c1c520e7cce5aa48d4d643
dragon-hex <infinityuser0101@gmail.com>
2021-11-01 15:32:57 -0300

    new release 1.1

--- kot2/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import audio
-from . import engine
-from . import system
-from . import ui
-from . import util
-from . import game
-# define the version of the library
-KOT2_VERSION = ["1.0", 10, 1.0]
