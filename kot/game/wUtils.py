829b7da0e7e971f4a755042d5f032c3341b2c8bb
dragon-hex <infinityuser0101@gmail.com>
2021-11-14 12:02:41 -0300

    new utility stuff

---
+++ kot/game/wUtils.py
@@ -0,0 +1,23 @@
+import random
+
+__uniqueKeysAlreadyGenerated = []
+def generateUniqueId(length=64):
+    ranges = [
+        (ord('a'),ord('z')),
+        (ord('A'),ord('Z')),
+        (ord('0'),ord('9')),
+    ]
+    whatId = ""
+    while True:
+        whatId = ""
+        for index in range(0,length):
+            whatRange   = random.randint(0,len(ranges)-1)
+            rangeSet    = ranges[whatRange]
+            whatId      += chr(random.randint(rangeSet[0],rangeSet[1]))
+        if whatId in __uniqueKeysAlreadyGenerated:
+            continue
+        else:
+            __uniqueKeysAlreadyGenerated.append(whatId)
+            break
+    print(__uniqueKeysAlreadyGenerated)
+    return whatId
