1a7976a8baeb64d6fbd3d4a327f9d60744a0b2d6
dragon-hex <infinityuser0101@gmail.com>
2021-11-14 12:02:32 -0300

    new world test for movement

--- kot/game/world.py
+++ kot/game/world.py
@@ -13,6 +13,9 @@ KEYS_DOWN       = [pygame.K_DOWN,   pygame.K_s]
 KEYS_RIGHT      = [pygame.K_RIGHT,  pygame.K_d]
 KEYS_LEFT       = [pygame.K_LEFT,   pygame.K_a]
 
+KEY_DEBUG_SCREEN            = pygame.K_F3
+KEY_DEBUG_ELEMENT_MOVEMENT  = pygame.K_F4
+
 KOT_ELEMENT_NAME            = 0
 KOT_ELEMENT_TILE_POSITION   = 1
 KOT_ELEMENT_ABS_POSITION    = 2
@@ -32,6 +35,9 @@ KOT_PLAYER_LOOK_RIGHT       = 3
 # -- project modifiers --
 
 DEBUG_THIS_FILE             = True
+
+# -- utils --
+from .wUtils import *
 
 # -- the main world stuff --
 class kotWorldStorage: 
@@ -58,6 +64,7 @@ class kotWorldStorage:
         self.camera     = [0, 0]
         self.pSpeed     = 4
         self.usingMove  = None
+        self.uniqueId   = generateUniqueId()
 
 class kotWorld:
     def __init__(self, kotSharedCore, kotSharedStorage, viewport):
@@ -88,6 +95,7 @@ class kotWorld:
         self.playerTexIndexT= 0
         # -- setup event --
         self.atWorldUpdate = None
+        self.__worldTestElementMovement = False
         self.debug.write("finished to build kotWorld class.")
     
     #
@@ -326,28 +334,36 @@ class kotWorld:
         elementRect             = pygame.Rect((elementPositionAbsolute[0],elementPositionAbsolute[1]),(elementSize[0], elementSize[1]))
         # setup the element here.
         world.elements.append([
-            # 0: element name
+            # 0: ELEMENT_NAME: this will store the element's name, on the script language
+            # or command, this is very important to identify a element name, for example:
+            # getElementByName("spike") -> return element_class<spike>
             elementName,
-            # 1: element position
+            # 1: ELEMENT_POSITION: the element initial position, in case of resets, this
+            # but the position to be drawn on the screen is on the ELEMENT_RECT, this is
+            # just for storage of the initial position!
             elementPosition,
-            # 2: element position (absolute)
+            # 2: ELEMENT_POSITION_ABS: the initial position of the element, this abs position
+            # is relative to the background world.
             elementPositionAbsolute,
-            # 3: elementTexture: stores the element texture.
-            # NOTE: even if the texture is a image, it needs to be inside a list!
+            # 3: ELEMENT_TEXTURE: the element texture, if sprite is stored inside a list
+            # and if it is a image, then it is stored in a single image!
             elementTexture,
-            # 4: elementTextureType: the type of texture to be used, 0: image, 1: sprite.
+            # 4: ELEMENT_TEXTURE_TYPE: the element texture type is what determines the
+            # element method to draw.
             elementTextureType,
-            # 5: elementTextureIndex (for sprites only): it will index the animation in
-            # the sprite list.
+            # 5: ELEMENT_SPRITE_INDEX: (FOR SPRITES ONLY): this will index the sprite animation.
             0,
-            # 6: elementTextureTimeIndex (for sprites only): it will index the time for the
-            # next animation to appear.
+            # 6: ELEMENT_SPRITE_INDEX_TIMER: (FOR SPRITES ONLY): this will count the time for the
+            # next animation to be set.
             0,
-            # 7: elementRect: for collision and etc.
+            # 7: ELEMENT_RECT: NOTE: this is the most important element inside the elements,
+            # basically this keep the element position & check for collisions.
             elementRect,
-            # 8: elementGenericName: some generic name for later getElementByGenericName()
+            # 8: ELEMENT_GENERIC_NAME: this is mostly of like a generic name for use on the
+            # getElementByGenericName("spike.centralspike0") -> return element_class<spike>
             elementGenericName,
-            # 9: elementTextureData: setup the data.
+            # 9: ELEMENT_GENERIC_DATA: to regenerate the data when the world switch is
+            # being set.
             elementTextureData
         ])
 
@@ -362,9 +378,9 @@ class kotWorld:
     #
     # -- tick the world --
     #
-    def moveElements(self, xDir, yDir):
+    def moveElements(self, forWorld, xDir, yDir):
         """moveElements: move all the elements."""
-        for element in self.world.elements:
+        for element in forWorld.elements:
             element[KOT_ELEMENT_RECT].x += xDir
             element[KOT_ELEMENT_RECT].y += yDir
     
@@ -376,35 +392,40 @@ class kotWorld:
                 return True
         return False
 
-    def move(self, xDir, yDir):
+    def move(self, forWorld, xDir, yDir, disableCollision=False):
         """move: move the camera + the player."""
         # test for possible collisions.
-        if self.moveTest(xDir, yDir):
-            return
+        if not disableCollision:
+            if self.moveTest(xDir, yDir):
+                return
         # move the world and the elements.
-        self.world.camera[0] += xDir
-        self.world.camera[1] += yDir
-        self.moveElements(xDir, yDir)
+        forWorld.camera[0] += xDir
+        forWorld.camera[1] += yDir
+        self.moveElements(forWorld, xDir, yDir)
     
-    def movePrecise(self, xDir, yDir):
+    def movePrecise(self, forWorld, xDir, yDir, disableCollision=False):
         """movePrecise: this is a more expansive move function, this will
         try to move precisely to the max possible direction."""
-        maxHitX = 0     ; isXNeg = (xDir < 0)
-        maxHitY = 0     ; isYNeg = (yDir < 0)
-        for hitX in range(0, -(xDir) if isXNeg else xDir):
-            if self.moveTest(-hitX if isXNeg else hitX, 0):
-                break
-            else:
-                maxHitX = -hitX if isXNeg else hitX
-        for hitY in range(0, -(yDir) if yDir < 0 else yDir): 
-            if self.moveTest(0, -hitY if isYNeg else hitY):
-                break
-            else:
-                maxHitY = -hitY if isYNeg else hitY
+        if not disableCollision:
+            maxHitX = 0     ; isXNeg = (xDir < 0)
+            maxHitY = 0     ; isYNeg = (yDir < 0)
+            for hitX in range(0, -(xDir) if isXNeg else xDir):
+                if self.moveTest(-hitX if isXNeg else hitX, 0):
+                    break
+                else:
+                    maxHitX = -hitX if isXNeg else hitX
+            for hitY in range(0, -(yDir) if yDir < 0 else yDir): 
+                if self.moveTest(0, -hitY if isYNeg else hitY):
+                    break
+                else:
+                    maxHitY = -hitY if isYNeg else hitY
+        else:
+            maxHitX = xDir
+            maxHitY = yDir
         # after all this stuff, move the world, case is possible.
-        self.world.camera[0] += maxHitX
-        self.world.camera[1] += maxHitY
-        self.moveElements(maxHitX,maxHitY)
+        forWorld.camera[0] += maxHitX
+        forWorld.camera[1] += maxHitY
+        self.moveElements(forWorld, maxHitX,maxHitY)
     
     def changePlayerLookAt(self, whatDirection):
         """changePlayerLookAt: changes and process the animation."""
@@ -420,10 +441,10 @@ class kotWorld:
             self.playerTexIndex     = 0
             self.playerTexIndexT    = 0
     
-    def updateWorldElements(self):
+    def updateWorldElements(self, world):
         """updateWorldElements: update all the world elements that can
         have a sprite, if it is a image, leave it."""
-        for element in self.world.elements:
+        for element in world.elements:
             if element[KOT_ELEMENT_TEXTURE_TYPE] == 1:
                 if element[KOT_ELEMENT_TEXTURE_TINDEX] < pygame.time.get_ticks():
                     spriteListSize = len(element[KOT_ELEMENT_TEXTURE])
@@ -439,45 +460,94 @@ class kotWorld:
         # mode.
         for event in eventList:
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_F2:
+                if event.key == KEY_DEBUG_SCREEN:
+                    # TODO: this will mostly like to be removed on the last version.
+                    # NOTE: it is just to test the multiple worlds that can be loaded.
                     self.changeWorld("level0.lirusekaa" if self.world.genericName != "level0.lirusekaa" else "level-1.lirusekaa")
+                if event.key == KEY_DEBUG_ELEMENT_MOVEMENT and DEBUG_THIS_FILE:
+                    # TODO: this will mostly like to be removed on the last version.
+                    self.__worldTestElementMovement = not self.__worldTestElementMovement
             if event.type == pygame.VIDEORESIZE:
+                # trigger all the element resize.
                 self.videoResizeEvent(event.w, event.h)
 
+        # 
+        # process the keys that are used for movement.
+        #
         keyPressed = pygame.key.get_pressed()
         if      keyPressed[KEYS_UP[0]]      or keyPressed[KEYS_UP[1]]:
             self.changePlayerLookAt(KOT_PLAYER_LOOK_UP)
-            self.usingMove(0,     self.world.pSpeed)
+            self.usingMove(self.world, 0,     self.world.pSpeed)
         elif    keyPressed[KEYS_DOWN[0]]    or keyPressed[KEYS_DOWN[1]]:
             self.changePlayerLookAt(KOT_PLAYER_LOOK_DOWN)
-            self.usingMove(0,    -self.world.pSpeed)
+            self.usingMove(self.world, 0,    -self.world.pSpeed)
         elif    keyPressed[KEYS_LEFT[0]]    or keyPressed[KEYS_LEFT[1]]:
             self.changePlayerLookAt(KOT_PLAYER_LOOK_LEFT)
-            self.usingMove(self.world.pSpeed,     0)
+            self.usingMove(self.world, self.world.pSpeed,     0)
         elif    keyPressed[KEYS_RIGHT[0]]   or keyPressed[KEYS_RIGHT[1]]:
             self.changePlayerLookAt(KOT_PLAYER_LOOK_RIGHT)
-            self.usingMove(-self.world.pSpeed,    0)
+            self.usingMove(self.world, -self.world.pSpeed,    0)
         
         # update the elements (sprites)
-        self.updateWorldElements()
+        self.updateWorldElements(self.world)
+        if self.__worldTestElementMovement:
+            self.test_MoveElements()
+    
+    #
+    # -- test functions --
+    #
+
+    def test_MoveElements(self):
+        """test function (moveElements): this will move the elements
+        around the map."""
+        movements = [1, -1, 2, -2, 3, -3, 4, -4, 5, -5, 6, -6]
+        for element in self.world.elements:
+            newX = element[KOT_ELEMENT_RECT].x + random.choice(movements)
+            if newX > 0 and newX < self.worldBackground.get_size()[0]:
+                element[KOT_ELEMENT_RECT].x = newX
+            newY = element[KOT_ELEMENT_RECT].y + random.choice(movements)
+            if newY > 0 and newY < self.worldBackground.get_size()[1]:
+                element[KOT_ELEMENT_RECT].y = newY
     
     #
     # -- video resize event & other subevents --
     #
+    def vre_RecalculateWorldsPlayerPosition(self, newSize):
+        """vre_RecalculateWorldsPlayerPosition: move all the player
+        positions on the other worlds, this is a very expansive process
+        and important.
+        """
+        worldKeys = list(self.worlds.keys())
+        for worldKey in worldKeys:
+            if self.world.uniqueId == self.worlds[worldKey].uniqueId:
+                # NOTE: pass because we already calculated this world
+                # position.
+                self.debug.write("not moving for world = %s" % self.worlds[worldKey].name)
+                pass
+            else:
+                self.debug.write("changing for world = %s | %s" % (self.worlds[worldKey].name, self.worlds[worldKey].uniqueId))
+                # move all the world elements + camera
+                self.worlds[worldKey].camera[0] += newSize[0]
+                self.worlds[worldKey].camera[1] += newSize[1]
+                for element in self.worlds[worldKey].elements:
+                    element[KOT_ELEMENT_RECT].x += newSize[0]
+                    element[KOT_ELEMENT_RECT].y += newSize[1]
+
     def vre_RecalculatePlayerPosition(self, prevSize, newSize):
         """vre: videoResizeEvent, this function will recalculate the player
         position, since the player is always on the center, it is very hard
         to check where the old position should be."""
         # NOTE: this worlds like this: calculate the number of spaces that has
         # changed from the last resize.
-        newSizeX, newSizeY              = newSize[0], newSize[1]
-        newPlayerPosX, newPlayerPosY    = (newSizeX // 2 - self.playerSize[0] // 2), (newSizeY // 2 - self.playerSize[1] // 2)
-        playerOldPos                    = (self.playerRect.x, self.playerRect.y)
-        self.playerRect.x, self.playerRect.y = newPlayerPosX, newPlayerPosY
-        self.move(
-            self.playerRect.x - playerOldPos[0],
-            self.playerRect.y - playerOldPos[1]
-        )
+        newSizeX, newSizeY                      = newSize[0], newSize[1]
+        newPlayerPosX, newPlayerPosY            = (newSizeX // 2 - self.playerSize[0] // 2), (newSizeY // 2 - self.playerSize[1] // 2)
+        playerOldPos                            = (self.playerRect.x, self.playerRect.y)
+        self.playerRect.x, self.playerRect.y    = newPlayerPosX, newPlayerPosY
+        posX, posY                              = self.playerRect.x - playerOldPos[0], self.playerRect.y - playerOldPos[1]
+        # NOTE: move this world camera & the other worlds
+        # this needs to be done because the camera is always on the center.
+        self.move(self.world, posX, posY)
+        self.vre_RecalculateWorldsPlayerPosition((posX, posY))
 
 
     def videoResizeEvent(self, newSizeX, newSizeY):
@@ -498,6 +568,7 @@ class kotWorld:
 
     def drawBackground(self):
         """drawBackground: keep the background on the back!"""
+        # setup the background.
         self.viewport.blit(self.worldBackground,self.world.camera)
     
     def drawElements(self):
