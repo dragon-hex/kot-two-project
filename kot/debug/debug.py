7b93de32e09892c3347aaf9d790a9f5cf470c057
dragon-hex <infinityuser0101@gmail.com>
2021-11-21 14:28:48 -0300

    debug message id now is showed using normal numbers.

--- kot/debug/debug.py
+++ kot/debug/debug.py
@@ -6,13 +6,15 @@ class kotDebug:
         self.name = logFrom
         self.logIndex = 0
         self.enabled = True
+
     def __get_timestamp(self):
         """ return a common timestamp. """
         return strftime("%Y/%m/%d %H:%M:%S",gmtime())
+
     def write(self, string):
         """write: write all your log!"""
         if self.enabled:
-            self.output.write("[time = '%s'] from = %s, log_index = 0x%0.4d: %s\n" %(
+            self.output.write("[time = '%s'] from: '%s', log_index: %.8d: %s\n" %(
                 self.__get_timestamp(),     # -> return the time;
                 self.name,                  # -> the package name;
                 self.logIndex,              # -> the message index;
