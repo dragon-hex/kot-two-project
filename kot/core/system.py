b36205866dc9f4a3bd8791f38621b4f16fe4edd3
dragon-hex <infinityuser0101@gmail.com>
2021-11-24 14:49:18 -0300

    new system functions

--- kot/core/system.py
+++ kot/core/system.py
@@ -7,21 +7,25 @@ class kotWindow:
         self.icon = None
         self.caption = None
         self.size = None
+
     def __applyTheIcon(self):
         """__applyTheIcon: basically applies the window icon."""
         if self.icon:
             try: pygame.display.set_icon(self.icon)
             except: return False
             return True
+
     def windowSetIcon(self, windowIcon):
         """windowSetIcon: set the window icon."""
         self.icon = windowIcon
         self.__applyTheIcon()
+
     def windowInit(self):
         """windowInit: initializes the window."""
         self.surface = pygame.display.set_mode(self.size,pygame.RESIZABLE)
         pygame.display.set_caption(self.caption)
         self.__applyTheIcon()
+
     def windowUpdateProperties(self):
         """windowUpdateProperties: update the window properties."""
         pygame.display.set_caption(self.caption)
@@ -33,10 +37,19 @@ class kotSharedCore:
         self.window = kotWindow()
         self.running = True
         self.mode = []
-        self.on_mode = 0
+        self.onMode = 0
         # for the statistics & debug gui's
         self.tickTime = 0
         self.drawTime = 0
+
+    def setupWindowByData(self, whatData):
+        """setupWindowByData: basic configures the data.
+        the icon needs to be already loaded as a surface!
+        See: provider.buildWindowData"""
+        self.window.icon = whatData['icon']
+        self.window.caption = whatData['title']
+        self.window.windowUpdateProperties()
+
     def getRunning(self):
         """getRunning: get the state of the game."""
         return self.running
